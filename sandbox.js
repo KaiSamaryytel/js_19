451224d53244a5ed658f6fd1b143b8617bcae384
Kai Samaryytel <kaikoppel@gmail.com>
2019-10-18 12:11:31 +0300

    first commit

---
+++ sandbox.js
@@ -0,0 +1,26 @@
+//strings
+console.log('hello, world');
+
+let email = 'minu@email.ee';
+console.log(email);
+
+//string concatenation
+let firstName = 'Eesnimi';
+let lastName = 'Perenimi';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[0]);
+
+//string lenght
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
