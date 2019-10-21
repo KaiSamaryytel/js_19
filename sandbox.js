92bcbdadb4482cef6d3f05b7fc40e30e155df9bb
Kai Samaryytel <kaikoppel@gmail.com>
2019-10-21 13:17:23 +0300

    Loops

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,15 @@
-// type conversion
-// let score = '100';
+// for loops
+// loop lõppu ;-ei käi!
+for(let i = 0; i < 5; i++){
+  console.log('loop: ', i);
+}
 
-// score = Number(score);
-// console.log(score + 1);
-// console.log(typeof score);
+console.log('loop finished');
 
-// let result = Number('hello');
-// let result = String(50);
-// let result = Boolean(100);
-// let result = Boolean(0);
-// let result = Boolean('0');
-let result = Boolean('');
+const names = ['mari', 'jüri', 'kristi'];
 
-console.log(result, typeof result);
+for(let i = 0; i < names.length; i++){
+  console.log(names[i]);
+  let html = `<div>${names[i]}</div>`;
+  console.log(html);
+}
