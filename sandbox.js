61616595342175f9f6453b2cd67910b19ab0ef65
Kai Samaryytel <kaikoppel@gmail.com>
2019-10-21 12:49:27 +0300

    Conversion

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,15 @@
-// comparisons
+// type conversion
+// let score = '100';
 
-let age = 25;
+// score = Number(score);
+// console.log(score + 1);
+// console.log(typeof score);
 
-// loose comparison (different types can still be equal)
+// let result = Number('hello');
+// let result = String(50);
+// let result = Boolean(100);
+// let result = Boolean(0);
+// let result = Boolean('0');
+let result = Boolean('');
 
-console.log(age == 25);
-console.log(age == '25');
-console.log(age != 25);
-console.log(age != '25');
-
-// strict comparison (different types cannot be equal)
-
-console.log(age === 25);
-console.log(age === '25');
-console.log(age !== 25);
-console.log(age !== '25');
+console.log(result, typeof result);
