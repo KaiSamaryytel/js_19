bf493d3e850e773c5388b322d9ce73a7be96c0b8
Kai Samaryytel <kaikoppel@gmail.com>
2019-10-21 12:31:16 +0300

    Booleans & Comparisons

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,32 @@
-// null & undefined
+// booleans & comparisons
 
-let age = null;
+// console.log(true, false, 'true', 'false');
 
-console.log(age, age + 3, `the age is ${age}`);
+// methods can return booleans
+// let email = 'mari@perenimi.ee';
+// let names = ['mari', 'jüri', 'mart'];
+
+// let result = email.includes('@');
+// let result = names.includes('mari');
+
+// console.log(result);
+
+
+// comparison operators
+let age = 25;
+
+console.log(age == 25);
+console.log(age == 30);
+console.log(age != 30);
+console.log(age > 20);
+console.log(age < 20);
+console.log(age <= 25);
+console.log(age >= 25);
+
+let name = 'shaun';
+
+console.log(name == 'shaun');
+console.log(name == 'Shaun');
+console.log(name > 'crystal');
+console.log(name > 'Shaun');
+console.log(name > 'Crystal');
