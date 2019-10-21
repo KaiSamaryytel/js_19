d165d73efb8f8196066e882431e9818e1dbe94fb
Kai Samaryytel <kaikoppel@gmail.com>
2019-10-21 12:15:36 +0300

    Null & Undefined

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,5 @@
-// arrays
+// null & undefined
 
-let liblikad = ['admiral', 'riidekoi', 'suur-punasuru'];
+let age = null;
 
-// näitab positsiooni. on võimalik asendada.
-// liblikad[1] = 'silmiksuru';
-// console.log(liblikad[1]);
-
-// let vanus = [20, 25, 30, 35];
-// console.log(vanus[2]);
-
-// let random = ['päevapaabusilm', 'nõgeseliblikas', 30, 20];
-// console.log(random);
-
-
-// array length
-// console.log(liblikad.length);
-
-// array methods
-
-// let result = liblikad.join(',');
-// let result = liblikad.indexOf('riidekoi');
-// let result = liblikad.concat(['admiral', 'nõgeseliblikas']);
-//let result = liblikad.push('rohetäpik');
-let result = liblikad.pop();
-
-console.log(result);
-console.log(liblikad);
+console.log(age, age + 3, `the age is ${age}`);
