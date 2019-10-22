a87df40a6ea0122bb4247b7ec0dd6c3d0ed89471
Kai Samaryytel <kaikoppel@gmail.com>
2019-10-22 11:05:47 +0300

    Switch statements

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,37 @@
-// break & continue
-const scores = [50, 25, 0, 30, 100, 20, 10];
+// switch statements
+const hinne = 'D';
 
-for(let i = 0; i < scores.length; i++){
-
-  if(scores[i] === 0){
-    continue;
-  }
-
-  console.log('your score:', scores[i]);
-
-  if(scores[i] === 100){
-    console.log('congrats, you got the top score!');
+switch(hinne){
+  case 'A':
+    console.log('Said hindeks A!');
     break;
-  }
-
+  case 'B':
+    console.log('Said hindeks B!');
+    break;
+  case 'C':
+    console.log('Said hindeks C!');
+    break;
+  case 'D':
+    console.log('Said hindeks D!');
+    break;
+  case 'E':
+    console.log('Said hindeks E!');
+    break;
+  default:
+    console.log('Tundmatu');
 }
+
+// using if statements
+// if(hinne === 'A'){
+
+// } else if(hinne === 'B'){
+
+// } else if(hinne === 'C'){
+
+// } else if(hinne === 'D'){
+
+// } else if(hinne === 'E'){
+
+// } else {
+
+// }
