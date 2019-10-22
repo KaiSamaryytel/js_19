6d270d0533976f098f0eb0d0455ec7d6c8709b34
Kai Samaryytel <kaikoppel@gmail.com>
2019-10-22 10:28:20 +0300

    If statements

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,20 @@
-// do while loops
-// loop lõppu } ;-ei käi!
+// if statements
+ const age = 25;
 
-let i = 5;
-
-do{
-  console.log('val of i is: ', i);
-} while(i < 5);
+ if(age > 20){
+   console.log('oled üle 20 aasta vana');
+ }
 
 
+const liblikad = ['admiral', 'aasaöölane', 'taevastiib', 'keldriöölane'];
 
+if(liblikad.length > 3){
+  console.log("nüüd on küll palju liblikaid");
+ }
 
+ 
+ const salasõna = 'p@ssword';
+
+if(salasõna.length >= 8){
+  console.log('salasõna on piisavalt pikk!');
+}
