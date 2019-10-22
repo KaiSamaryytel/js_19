ea4e45d78863c19670925dbee988d1c49120c868
Kai Samaryytel <kaikoppel@gmail.com>
2019-10-22 10:49:38 +0300

    Logical operators OR and AND

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,12 @@
-// if statements
- const age = 25;
+ // logical operators - OR || and AND &&
 
- if(age > 20){
-   console.log('oled üle 20 aasta vana');
- }
+ const salasõna = 'p@ssword';
 
-
-const liblikad = ['admiral', 'aasaöölane', 'taevastiib', 'keldriöölane'];
-
-if(liblikad.length > 3){
-  console.log("nüüd on küll palju liblikaid");
- }
-
- // else statements
-
- const salasõna = 'p@sswordstrong';
-
- if(salasõna.length >= 12){
+ if(salasõna.length >= 12 && salasõna.includes('@')){
   console.log('salasõna on väga tugev!');
 
- } else if(salasõna.length >= 8){
-  console.log('salasõna on piisavalt pikk!');
+ } else if(salasõna.length >= 8 || salasõna.includes('@')){
+  console.log('salasõna on piisavalt tugev!');
  } else{
-  console.log('salasõna ei ole piisavalt pikk!');
+  console.log('salasõna ei ole piisavalt tugev!');
 }
