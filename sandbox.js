05735f9e6dcb764fcab772686167de6d5b28ed20
Kai Samaryytel <kaikoppel@gmail.com>
2019-10-23 12:15:30 +0300

    (Practise) arrow functions

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,39 @@
-// returning statements
+ // arrow functions
 
-const calcArea = function(radius){
-  return 3.14 * radius**2;
-};
+// regular function
+// const calcArea = function(radius){
+//   return 3.14 * radius**2;
+// }
+
+// arrow function
+const calcArea = radius => 3.14 * radius**2;
 
 const area = calcArea(5);
-console.log('Area is:', area);
+console.log('area is:', area);
+
+// practise arrow functions
+
+// const greet = function(){
+//   return 'hello, world';
+// }
+
+const greet = () => 'hello, world';
+
+// const bill = function(products, tax){
+//   let total = 0;
+//   for(let i = 0; i < products.length; i++){
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// }
+
+const bill = (products, tax) => {
+  let total = 0;
+  for(let i = 0; i < products.length; i++){
+    total += products[i]  + products[i] * tax;
+  }
+  return total;
+}
+
+console.log(greet());
+console.log(bill([10,15,30], 0.2));
